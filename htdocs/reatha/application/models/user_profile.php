2fbdeed6e9ebe0291bf82c3442b440a5d2146bd8
vheinitz <vheinitz@googlemail.com>
2013-01-30 22:33:57 +0100

    Fixed Table name(not caseinsensitive on reatha.de)

--- htdocs/reatha/application/models/user_profile.php
+++ htdocs/reatha/application/models/user_profile.php
@@ -1,6 +1,6 @@
 <?php
 class User_profile extends Datamapper{
-    var $table = "User_profiles";
+    var $table = "user_profiles";
     var $has_one = array('user');
     var $auto_populate_has_one = TRUE;
     var $auto_populate_has_many = TRUE;
