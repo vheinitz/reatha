532811372554ee26da7ed1f128604c714d0512c1
Nicu Burac <nicu@bertieandbean.com>
2013-02-04 16:36:27 +0200

    attempt to ignore database config, general config and index.php

--- htdocs/reatha/application/config/config.php
+++ htdocs/reatha/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '/htdocs/reatha';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
