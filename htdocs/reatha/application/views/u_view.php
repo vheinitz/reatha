79e394430d5b20f577eca994b022bca5cf3a8abd
vheinitz <vheinitz@googlemail.com>
2013-03-01 22:17:22 +0100

    Small change in device-list view.

--- htdocs/reatha/application/views/u_view.php
+++ htdocs/reatha/application/views/u_view.php
@@ -3,29 +3,34 @@
 <div class="row">
 	<div class="span12" id="user-devices-list">
 		<table class="table table-striped table-bordered">
-		  	<thead>
+		  	<!-- thead>
 			    <tr>
 				    <th style="text-align: center; width:40%">Devices:</th>
 			    </tr>
-			</thead>
+			</thead -->
 			<tbody> 
-		<?php foreach($devices as $device){
+		<?php 
+		
+			$firstRow=1;
+			foreach($devices as $device){
 /*			$onclick=""; $class=""; 
 			if($device->has_main_view()){
 				$onclick = "onclick=\"window.location.href = '".base_url()."u/device/$device->id'\"";
 				$class = "class = 'user-device-has-view'";
 			}*/
+			if(!$firstRow) echo "<tr><td>&nbsp;</td></tr>";
+			$firstRow=0;
 			?>
 		<tr>
 			<td>
 				<div id="user-device-wrapper" onclick="window.location.href='<?php echo base_url()."u/device/$device->id"; ?>'">
-					<?php echo "<b>Name: </b>".$device->name; ?><br/>
+					<?php echo "<b>".$device->name."</b>"; ?><hr/>
 					<?php echo "<b>Location: </b>".$device->location; ?><br/>
 					<b>Power: </b><span id="power_<?php echo $device->id; ?>"></span><br/>
 				</div>
 				<a class="btn" id="user-device-notification-setup" href="<?php echo base_url(); ?>u/notifications/<?php echo $device->id; ?>" ><i class="icon-envelope"></i> Notifications</a>		
 			</td>
-		</tr>	
+		</tr>			
 		<?php } ?>
 		</tbody>
 		</table>
