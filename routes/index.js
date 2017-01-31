c5ab29e830e6488db4522d78122b3b7e280c0b86
Valentin Heinitz <vheinitz@googlemail.com>
2017-01-31 22:54:12 +0100

    Using lokijs

--- routes/index.js
+++ routes/index.js
@@ -8,30 +8,103 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json({ type: 'application/*+json' } );
 var assert = require('assert');
+var lokijs = require('lokijs');
 var router = express.Router();
 
-var devicesData=[];
-var instruments=
-	[
-		{ key:"u1/device_1"}
-		,{ key:"u1/device_2"}
-		,{ key:"u1/device_3"}
-		,{ key:"u2/device_1"}
-		,{ key:"u2/device_2"}
-		,{ key:"u2/device_3"}
-		,{ key:"u3/device_1"}
-		,{ key:"u3/device_2"}
-		,{ key:"u3/device_3"}
-	];
 
-var sessions=
-	[
-		{ id:"u1", user: "u1"}
-	];
+var db = new lokijs('loki.json', 
+{
+	autosave: true, 
+	autosaveInterval: 60000,
+	autoload: true,
+    autoloadCallback : initDB
+});
 
-function userOfSession(session_id) {
-	return "u1";
+var users=null;
+var devices=null;
+var sessions=null;
+
+
+function test()
+{
+
+	s = sessionStart( "u1" );
+	console.log( "S ", JSON.stringify( s  ) )
+	u = sessionUser(s)
+	console.log( "U ",JSON.stringify(  u  ) )
+	
+	devs = userDevices(u)
+	console.log( "Devices ",JSON.stringify( devs  ) )
+
 }
+
+function initDB()
+{		
+    console.log( "initDB" )
+	
+	var coll = db.getCollection('users');
+    if (coll === null) {
+		console.log ("Creating users")
+        users = db.addCollection('users')
+		users.insert({name:'u1', password: '1'})
+		users.insert({name:'u2', password: '2'})
+		users.insert({name:'u3', password: '3'})
+    }
+    
+	coll = db.getCollection('devices');
+    if (coll === null) {
+		console.log ("Creating devices")
+        devices = db.addCollection('devices')
+		devices.insert({key:'d1', name:'Device1', location:"floor 1", users:['u1'], status: 'OK', current_wl:'123'})
+		devices.insert({key:'d2', name:'Device2', location:"floor 2", users:['u1','u2'], status: 'OK', current_wl:'123'})
+		devices.insert({key:'d3', name:'Device3', location:"floor 3", users:['u2','u3'], status: 'OK', current_wl:'123'})
+		devices.insert({key:'d4', name:'Device4', location:"floor 4", users:['u1','u2','u3','u4'], status: 'OK', current_wl:'123'})
+    }
+	
+	coll = null;//db.getCollection('sessions');
+    if (coll === null) {
+		console.log ("Creating sessions")
+        db.addCollection('sessions')			
+    }
+	users = db.getCollection('users')
+    devices = db.getCollection('devices')
+    sessions = db.getCollection('sessions')
+	sessions.clear();
+
+	test();
+}
+
+
+function sessionUser(session_id) {
+	console.log( "sessionUser( ", session_id , ")" )
+	s = sessions.where( function(obj){
+		return obj.session_id == session_id;
+	})
+	console.log( JSON.stringify(s) )
+	return s[0].user;	
+}
+
+function sessionStart( user ) {
+	s = sessions.where( function(obj){	
+	  return obj.user == user;
+	})
+	
+	if (s.length) throw("Session for user already exists")
+		
+	var session_id = 's_'+user;
+	sessions.insert({ user:user, session_id:session_id})	
+		
+	return session_id;	
+}
+
+function userDevices(user) {
+	console.log(  "userDevices", user )
+	devs = devices.where( function(obj){
+		return obj.users.indexOf( user ) > -1;
+	}) 	
+	return devs;	
+}
+
 
 
 /* GET home page. */
@@ -43,8 +116,8 @@ router.get('/', function(req, res, next) {
 
 router.post('/api/auth/login/:user/:passwd', function(req, res) {
   console.log('/api/auth/login/:'+req.params.user+'/:'+req.params.passwd)
-  var results = { status: "ERROR", data:"err descr" };
-  results = {"status":"OK","data":{"session_id":"u1","level":req.params.user}};
+  var ses = sessionStart( req.params.user );
+  results = {"status":"OK","data":{"session_id":ses}};
   return res.json(results);
 });
 
@@ -61,19 +134,15 @@ router.post('/api/instrument/list/:session_id', function(req, res) {
 	
 	
 	try{
-		user = userOfSession( req.params.session_id );
+		user = sessionUser( req.params.session_id );
+		
+		console.log('user:', user);
 		if ( user === '' )
 			throw("Session Error");
-			 
-		var filename = "frontend/data/" + user + "/devices.json";
-		console.log(filename);
-		fs.readFile(filename,'utf8', function read(err, data) {
-			if (err) {
-				throw err;
-			}
-			console.log( "Data:", data );			
-			return res.end(data);
-		});
+			
+		devs =  userDevices( user )
+		
+		return res.end( JSON.stringify( devs ) )
 	}
 	catch(exception)
 	{
