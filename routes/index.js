616301ad5e41164e8604566952453d808055aeb8
Valentin Heinitz <vheinitz@googlemail.com>
2017-02-03 00:52:22 +0100

    WORKS!!!

--- routes/index.js
+++ routes/index.js
@@ -44,7 +44,7 @@ function initDB()
 	
 	var coll = db.getCollection('users');
     if (coll === null) {
-		console.log ("Creating users")
+		console.log ("   Creating users")
         users = db.addCollection('users')
 		users.insert({name:'u1', password: '1'})
 		users.insert({name:'u2', password: '2'})
@@ -53,17 +53,17 @@ function initDB()
     
 	coll = db.getCollection('devices');
     if (coll === null) {
-		console.log ("Creating devices")
+		console.log ("   Creating devices")
         devices = db.addCollection('devices')
-		devices.insert({id:1, key:'d1', name:'Device1', location:"floor 1", users:['u1'], status: 'OK', current_wl:'123'})
-		devices.insert({id:2, key:'d2', name:'Device2', location:"floor 2", users:['u1','u2'], status: 'OK', current_wl:'123'})
-		devices.insert({id:3, key:'d3', name:'Device3', location:"floor 3", users:['u2','u3'], status: 'OK', current_wl:'123'})
-		devices.insert({id:4, key:'d4', name:'Device4', location:"floor 4", users:['u1','u2','u3','u4'], status: 'OK', current_wl:'123'})
+		devices.insert({id:'1', key:'d1', name:'Device1', location:"floor 1", users:['u1'], status: 'OK', data:{}})
+		devices.insert({id:'2', key:'d2', name:'Device2', location:"floor 2", users:['u1','u2'], status: 'OK', data:{}})
+		devices.insert({id:'3', key:'d3', name:'Device3', location:"floor 3", users:['u2','u3'], status: 'OK', data:{}})
+		devices.insert({id:'4', key:'d4', name:'Device4', location:"floor 4", users:['u1','u2','u3','u4'], status: 'OK', data:{}})
     }
 	
 	coll = null;//db.getCollection('sessions');
     if (coll === null) {
-		console.log ("Creating sessions")
+		console.log ("   Creating sessions")
         db.addCollection('sessions')			
     }
 	users = db.getCollection('users')
@@ -83,6 +83,7 @@ function sessionUser(session_id) {
 	console.log( JSON.stringify(s) )
 	return s[0].user;	
 }
+
 
 function sessionStart( user ) {
 	console.log( "## sessionStart( ", user , ")" )
@@ -211,57 +212,48 @@ router.post('/api/instrument/view/:session_id/:id', function(req, res) {
 
 router.post('/api/instrument/data/:session_id/:id', function(req, res) {
 	console.log('---/api/instrument/data/', req.params.session_id,'/', req.params.id)
-	devid =  JSON.stringify(2)
-	console.log(devid);
-	dev = devices.findOne( { 'id': devid } )
-	console.log(dev);
-	return res.end( JSON.stringify( dev ) )
 	
-    var filename = "frontend/data/u1/device_1.json";
-	console.log(filename);
+	
+  
 	try{
-		fs.readFile(filename,'utf8', function read(err, data) {
-			if (err) {
-				throw err;
-			}
-			//console.log( "Data:", data );			
-			return res.end(data);
-		});
+		
+		dev = devices.findOne( { 'id': req.params.id } )
+		//console.log('   Devices:',  JSON.stringify(devices) )
+		//console.log('   Device:',  JSON.stringify(dev) )
+		console.log(   "Device data: ", dev.data);
+		return res.end( JSON.stringify( dev.data ) )
 	}
 	catch(exception)
 	{
 		console.log( "Ex:", exception );
+		return res.json({ status: "ERROR", data:"not handled" });
 	}
-	return res.json({ status: "ERROR", data:"not handled" });
+	
 });
 
 
 
 /*
-curl -X POST -H "Content-Type: application/json" -d "{\"name\":\"Device X\", \"status\":\"ERR\", \"current_wl\":\"123\"}" 127.0.0.1/api/instrument/set/u1__device_1
+c:\bin\curl -X POST -H "Content-Type: application/json" -d "{\"name\":\"Device 4\", \"status\":\"OK\", \"current_wl\":\"123\"}" 127.0.0.1/api/instrument/set/d4
 */
 router.post('/api/instrument/set/:key', function(req, res) {
-	console.log('---/api/instrument/set/:key', req)
+	console.log('---/api/instrument/set/', req.params.key)
 	
-    var filename = "frontend/data/" + req.params.key.replace(/__/g, '/') + ".json";
-	console.log(filename);
+  
 	try{
-		data=JSON.stringify(req.body);
-		fs.writeFile( filename, data, function(err) {
-			if(err) {
-				var results = { status: "ERROR", data: err };
-				return res.json(results);
-			}
-			console.log("data saved!");
-			//console.log( "Data:", data );			
-			return res.json( { status: "OK" } );
-		}); 
+		dev  =  devices.findOne( { 'key' : req.params.key } )
+		console.log('   Device:',  JSON.stringify(dev) )
+		console.log('   Body:', JSON.stringify(req.body) )
+		dev.data = req.body
+		devices.update(dev);
+		return res.json( { status: "OK" } );
 	}
 	catch(exception)
 	{
-		console.log( "Ex:", exception );		
+		console.log( "Ex:", exception );
+		return res.json({ status: "ERROR", data:"not handled" });	
 	}
-	return res.json({ status: "ERROR", data:"not handled" });
+	
 });
 
   
